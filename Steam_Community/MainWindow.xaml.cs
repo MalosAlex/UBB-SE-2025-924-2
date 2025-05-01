8395aa384c4cbdf40cbf7af4518cb59bf52c3c81
Robert George Leustean <147397130+914RobbLeustean@users.noreply.github.com>
2025-05-01 20:21:11 +0300

    Add files via upload
    Migrate from single page to multi-page architecture by converting UserControls to Pages

--- Steam_Community/MainWindow.xaml.cs
+++ Steam_Community/MainWindow.xaml.cs
@@ -1,46 +1,27 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
-using News;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Search;
 using Steam_Community.DirectMessages.Views;
 using Steam_Community.DirectMessages.Models;
-using System.Diagnostics;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Search;
 
 namespace Steam_Community
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-            this.searchControl.ChatRoomOpened += HandleChatInvite;
-            this.Closed += this.searchControl.OnClosing;
+            this.searchPage.ChatRoomOpened += HandleChatInvite;
+            this.Closed += this.searchPage.OnClosing;
         }
 
-        public void HandleChatInvite(object? sender, ChatRoomOpenedEventArgs e)
+        public void HandleChatInvite(object sender, ChatRoomOpenedEventArgs e)
         {
             ChatRoomWindow chatRoomWindow = new ChatRoomWindow(e.Username, e.IpAddress);
             if (e.IpAddress == ChatConstants.HOST_IP_FINDER)
             {
-                chatRoomWindow.Closed += this.searchControl.StoppedHosting;
+                chatRoomWindow.Closed += this.searchPage.StoppedHosting;
             }
             chatRoomWindow.Activate();
         }
