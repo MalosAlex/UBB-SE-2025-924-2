c8800b0a996c64bf9bb15801925ee1b57d58137d
Info1technologies <marcuionut1504@gmail.com>
2025-05-05 14:29:18 +0300

    Stylecop

--- BusinessLayer/Models/Post.cs
+++ BusinessLayer/Models/Post.cs
@@ -5,20 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessLayer.Models
+{
 
-public class Post
-{
-    public int Id { get; set; }
-    public int AuthorId { get; set; }
-    public string Content { get; set; }
-    public DateTime UploadDate { get; set; }
-    public int NrLikes { get; set; }
-    public int NrDislikes { get; set; }
-    public int NrComments { get; set; }
-    public bool? ActiveUserRating { get; set; }
-}
-public static class PostRatingType
-{
-    public const bool LIKE = true;
-    public const bool DISLIKE = false;
+    public class Post
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Content { get; set; }
+        public DateTime UploadDate { get; set; }
+        public int NrLikes { get; set; }
+        public int NrDislikes { get; set; }
+        public int NrComments { get; set; }
+        public bool? ActiveUserRating { get; set; }
+    }
+    public static class PostRatingType
+    {
+        public const bool LIKE = true;
+        public const bool DISLIKE = false;
+    }
 }
