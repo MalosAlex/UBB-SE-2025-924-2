d497d634c4f5091db8c1706200ef8ec780971990
Ghestr <malosalexandru2004@gmail.com>
2025-05-10 19:10:35 +0300

    Tried some things

--- SteamWebApi/Program.cs
+++ SteamWebApi/Program.cs
@@ -1,5 +1,9 @@
 using BusinessLayer.Services.Interfaces;
 using BusinessLayer.Services;
+using Microsoft.EntityFrameworkCore;
+using BusinessLayer.Repositories.Interfaces;
+using BusinessLayer.Repositories;
+using BusinessLayer.DataContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +16,19 @@ builder.Services.AddScoped<IWalletService, WalletService>();
 builder.Services.AddScoped<ICollectionsService, CollectionsService>();
 builder.Services.AddScoped<IFeaturesService, FeaturesService>();
 builder.Services.AddScoped<IReviewService, ReviewService>();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IFriendRequestRepository, FriendRequestRepository>();
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
+builder.Services.AddScoped<ICollectionsRepository, CollectionsRepository>();
+builder.Services.AddScoped<IFeaturesRepository, FeaturesRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
