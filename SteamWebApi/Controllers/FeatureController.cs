8de13f4b8539894fa86b80df8deb1e328af24483
Paul Hiticas <hiticaspaul@gmail.com>
2025-05-10 15:01:13 +0300

    add asp.net project and create controllers

---
+++ SteamWebApi/Controllers/FeatureController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Models;
+using BusinessLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SteamWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IFeaturesService featuresService;
+
+        public FeatureController(IFeaturesService featuresService)
+        {
+            this.featuresService = featuresService;
+        }
+
+        [HttpGet]
+        public IActionResult GetFeatures()
+        {
+            var featuresByCategories = featuresService.GetFeaturesByCategories();
+            return Ok(featuresByCategories);
+        }
+
+        [HttpPost]
+        public IActionResult AddFeature(Feature feature)
+        {
+            return BadRequest("AddFeature method is not implemented in IFeaturesService.");
+        }
+    }
+}
---
+++ SteamWebApi/Controllers/WalletController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SteamWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WalletController : ControllerBase
+    {
+        private readonly IWalletService walletService;
+
+        public WalletController(IWalletService walletService)
+        {
+            this.walletService = walletService;
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult GetWallet(int userId)
+        {
+            var balance = walletService.GetBalance();
+            var points = walletService.GetPoints();
+
+            var wallet = new
+            {
+                UserId = userId,
+                Balance = balance,
+                Points = points
+            };
+
+            return Ok(wallet);
+        }
+
+        [HttpPost("add-money")]
+        public IActionResult AddMoney(int userId, decimal amount)
+        {
+            walletService.AddMoney(amount);
+            return Ok();
+        }
+    }
+}
