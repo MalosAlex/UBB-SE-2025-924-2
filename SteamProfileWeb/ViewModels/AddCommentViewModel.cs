fd585fc053ab18b841d42964dd7d421ccf06e6e7
Robert George Leustean <147397130+914RobbLeustean@users.noreply.github.com>
2025-05-14 01:10:51 +0300

    f*ck this bs forum ! MVC done (hopefully)

---
+++ SteamProfileWeb/ViewModels/AddCommentViewModel.cs
@@ -0,0 +1,14 @@
+﻿// AddCommentViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace SteamProfileWeb.ViewModels
+{
+    public class AddCommentViewModel
+    {
+        [Required]
+        public int PostId { get; set; }
+
+        [Required(ErrorMessage = "Comment content is required")]
+        public string Content { get; set; }
+    }
+}
---
+++ SteamProfileWeb/ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,17 @@
+﻿// CreatePostViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace SteamProfileWeb.ViewModels
+{
+    public class CreatePostViewModel
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; }
+
+        public int? GameId { get; set; }
+    }
+}
