67a9d9ec709ac1ebcc8b4113d96bb94e90b69402
Robistyle <robi9.lel@gmail.com>
2025-05-14 19:50:46 +0300

    Finished the mvc for achievements

---
+++ SteamProfileWeb/ViewModels/AchievementsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Models;
+
+namespace SteamProfileWeb.ViewModels
+{
+    public class AchievementsViewModel
+    {
+        public List<AchievementWithStatus> FriendshipsAchievements { get; set; } = new();
+        public List<AchievementWithStatus> OwnedGamesAchievements { get; set; } = new();
+        public List<AchievementWithStatus> SoldGamesAchievements { get; set; } = new();
+        public List<AchievementWithStatus> NumberOfPostsAchievements { get; set; } = new();
+        public List<AchievementWithStatus> NumberOfReviewsGivenAchievements { get; set; } = new();
+        public List<AchievementWithStatus> NumberOfReviewsReceivedAchievements { get; set; } = new();
+        public List<AchievementWithStatus> YearsOfActivityAchievements { get; set; } = new();
+        public List<AchievementWithStatus> DeveloperAchievements { get; set; } = new();
+    }
+
+}
